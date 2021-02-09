28c16e3a809052f9b2447431b865dabccce4b6b7
Mateus Melo <msmelo95@gmail.com>
2021-02-08 23:37:54 -0300

    Adicionando arquivos

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+  render() {
+    return <h1>My App</h1>;
+  }
+}
